a6a532af0ca95bb3b7f7be1f12250b8536ade6fc
Nobuyuki Fujioka <nobuyukifujioka@nobuyukis-mbp.home>
2018-06-06 00:03:28 +0100

    adds input validations

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Formik Practice
 
+formik library is found here:
+https://github.com/jaredpalmer/formik
 
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { withFormik, Form, Field } from 'formik'
-import Yup from 'yup'
+import * as yup from 'yup'
 
 const App = ({  //props is passed here from mapPropsToValue
   values,
+  errors,
+  touched,  //check if the input is already visited
+  isSubmitting
   // handleChange,
   // handleSubmit
 })=>(
@@ -13,8 +16,15 @@ const App = ({  //props is passed here from mapPropsToValue
   //   <button>Submit</button>
   // </form>
   <Form>
-    <Field type="email" name="email" placeholder="Email" />
-    <Field type="password" name="password" placeholder="Password" />
+    <div>
+      { touched.email && errors.email && <p>{errors.email}</p> }
+      <Field type="email" name="email" placeholder="Email" />
+    </div>
+    
+    <div>
+      { touched.password && errors.password && <p>{errors.password}</p> }
+      <Field type="password" name="password" placeholder="Password" />
+    </div>
     <label>
       <Field type="checkbox" name="newsletter" checked={values.newsletter} />
       Join our newsletter
@@ -23,7 +33,7 @@ const App = ({  //props is passed here from mapPropsToValue
       <option value="free">Free</option>
       <option value="premium">Premium</option>
     </Field>
-    <button>Submit</button>
+    <button disabled={isSubmitting}>Submit</button>
   </Form>
 )
 
@@ -36,14 +46,27 @@ const FormikApp = withFormik({
       plan: plan || 'free'
     }
   },
-  handleSubmit(values) {
-    console.log(values)
+  validationSchema: yup.object().shape({
+    email: yup.string().email('email not valid').required('email is required'),
+    password: yup.string().min(9, 'Password must be 9 characters or longer').required('Password is required')
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    setTimeout(()=>{
+      if(values.email=== 'test@test.io') {
+        setErrors({ email: 'That email is already taken.'})
+      } else {
+        console.log(values)
+        resetForm()
+      }
+      setSubmitting(false) //setSubmitting prop to false
+    }, 2000)
   }
 })(App)
 
 const ParentApp = (props)=>{
   return (
-    <FormikApp email="test@test.com" />
+    // <FormikApp email="test@test.com" />
+    <FormikApp />
   )
 }
 
